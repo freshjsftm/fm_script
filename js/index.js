01c8dfed5b04adddd1fa697a49531847207dc475
Elena <freshjs.ftm@gmail.com>
2021-10-13 09:49:40 +0300

    Add old syntax

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+
+const obj = {
+  prop:1,
+};
+const obj2 = new Object({prop:1});
+
+//constructor
---
+++ js/oldSyntax.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function User(name, sname, age){
+  this.name = name;
+  this.sname = sname;
+  this.age = age;
+}
+
+User.amount = 0;
+User.createTest = function(){
+  return new User('anonim', 'anonimovich', 55);
+}
+
+function UserProto(){
+  this.getFullName = function(){
+    return `${this.name} ${this.sname}`;
+  }
+}
+
+User.prototype = new UserProto();
+
+
+const u1 = new User('Elon', 'Musk', 50);
