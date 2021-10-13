efc8a98768faca0e2036616dd0dcb786d7aca165
Elena <freshjs.ftm@gmail.com>
2021-10-13 09:36:28 +0300

    Refactor project

--- js/index.js
+++ js/index.js
@@ -1,16 +1,1 @@
 'use strict';
-
-
-const pow = (num, exp)=>{
-  if(typeof num !=='number' || typeof exp !=='number'){
-    return new TypeError('Value must be number');
-  }
-  if(exp<0){
-    return new RangeError('Invalid value exp. Exp must >0');
-  }
-  if(exp===0){
-    return 1;
-  }
-  return num*pow(num, exp-1);
-}
-console.log(pow(2, -2));
