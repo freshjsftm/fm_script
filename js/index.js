1847cb22422b7e46aa1a51203085b505de80fb5e
Elena <freshjs.ftm@gmail.com>
2021-10-20 10:25:49 +0300

    Add example BIG O

--- js/index.js
+++ js/index.js
@@ -1,34 +1,54 @@
 'use strict';
 
-const myArray1 = new MyArray(1,undefined,1,new MyArray(2,new MyArray(3,new MyArray(4,4,4),3,3),2,2),1,1);
+const arr = [1,5,3,5,89,8,4,56,8,9,2,1,5];
+//константная сложность  O(1)
+console.log(arr[4])
 
-// const myArray2 = myArray1.flat(2);
-// console.log(myArray2);
-
-
-
-const mySymbol1 = Symbol('Property');
-const mySymbol2 = Symbol('Метка только для вас');
-
-const object = {
-  login:'qwerty',
-  mySymbol1: 'asda',
-  [mySymbol1]:123,
-};
-
-delete object[mySymbol1];
-
-
-const arr = [1,2,3,4,5];
-for (const iterator of arr) {
-  console.log(iterator)
+//линейная сложность   O(n)
+const linearSearch = (arr, key)=>{
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] === key){
+      return i;
+    }
+  }
+  return -1;
 }
 
-//console.log(...arr)
-const myArray2 = new MyArray(10,20,30)
-console.log(...myArray2)
-
-for (const iterator of myArray2) {
-  console.log(iterator)
+//квадратичная сложность   O(n^2)
+const createTableMult = (limit)=>{
+  const table = [];
+  for(let i=1; i<=limit; i++){
+    for(let j=1; j<=limit; j++){
+      table.push(`${i}*${j}=${i*j}`);
+    }
+  }
+  return table;
 }
+//console.log(createTableMult(2));
 
+
+//логарифмическая сложность   O(logn)
+
+const sortedArr = [1,3,5,8,9,12,15,19,40];
+
+const binarySearch = (arr, key)=>{
+  let start = 0;
+  let end = arr.length-1;
+  let middle;//index
+  while(start<=end){
+    middle = Math.round((start+end)/2);
+    if(arr[middle]===key){
+      return middle;
+    }
+    if(arr[middle]<key){
+      start = middle + 1;
+    }else{
+      end = middle - 1;
+    }
+  }
+  return -1;
+}
+console.log(binarySearch(sortedArr,8));
